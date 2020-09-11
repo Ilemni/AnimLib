57ecaecb5ce81ca2d9e9cf3b449ac4e89fea2d05
TwiliChaos <zundisplayed@gmail.com>
2020-09-11 14:33:57 -0700

    Add AnimationSource field to Track This is required for an improved Track.Range construction

--- Animations/AnimationSource.cs
+++ Animations/AnimationSource.cs
@@ -58,6 +58,57 @@ namespace AnimLib.Animations {
 
 
     /// <summary>
+    /// Creates a track with <see cref="LoopMode.Always"/> and <see cref="Direction.Forward"/>, with a <see cref="Frame"/> array ranging from <paramref name="start"/> to <paramref name="end"/>.
+    /// <para>The range is created along the Y axis, going downward.</para>
+    /// </summary>
+    /// <inheritdoc cref="Range(LoopMode, Direction, Frame, Frame)"/>
+    protected Track Range(Frame start, Frame end) => Range(LoopMode.Always, Direction.Forward, start, end);
+
+    /// <summary>
+    /// Creates a track with the given <see cref="LoopMode"/> and using <see cref="Direction.Forward"/>, with a <see cref="Frame"/> array ranging from <paramref name="start"/> to <paramref name="end"/>.
+    /// <para>The range is created along the Y axis, going downward.</para>
+    /// </summary>
+    /// <inheritdoc cref="Range(LoopMode, Direction, Frame, Frame)"/>
+    protected Track Range(LoopMode loopMode, Frame start, Frame end) => Range(loopMode, Direction.Forward, start, end);
+
+    /// <summary>
+    /// Creates a track with the given <see cref="LoopMode"/> and <see cref="Direction"/>, with a <see cref="Frame"/> array ranging from <paramref name="start"/> to <paramref name="end"/>.
+    /// <para>The range is created along the Y axis, going downward.</para>
+    /// </summary>
+    /// <param name="loopMode"><see cref="LoopMode"/> of the track.</param>
+    /// <param name="direction"><see cref="Direction"/> of the track.</param>
+    /// <param name="start">First <see cref="Frame"/> of the track.</param>
+    /// <param name="end">Last <see cref="Frame"/> of the track. Must be in the same column as and below <paramref name="start"/>.</param>
+    /// <returns>A new <see cref="Track"/> with the frames ranging from <paramref name="start"/> to <paramref name="end"/>.</returns>
+    /// <exception cref="ArgumentException">The X values of <paramref name="start"/> and <paramref name="end"/> must be equal.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">The Y value of <paramref name="start"/> must be less than the Y value of <paramref name="end"/>.</exception>
+    protected Track Range(LoopMode loopMode, Direction direction, Frame start, Frame end) {
+      // Fill range of frames
+      // I.e. if given [(0,1), (0,4)], we make [(0,1), (0,2), (0,3), (0,4)]
+      if (start.tile.X != end.tile.X) {
+        throw new ArgumentException($"The X values of {nameof(start)} and {nameof(end)} must be equal, instead got {start.tile.X}, {end.tile.X}.");
+      }
+      if (start.tile.Y >= end.tile.Y) {
+        throw new ArgumentOutOfRangeException($"The Y value of {nameof(start)} must be less than the Y value of {nameof(end)}, instead got {start.tile.Y}, {end.tile.Y}.");
+      }
+      var frames = new List<IFrame>();
+      for (int y = start.tile.Y; y < end.tile.Y; y++) {
+        frames.Add(new Frame(start.tile.X, y, start.duration));
+      }
+      frames.Add(end);
+      var track = new Track(this, loopMode, direction, frames.ToArray());
+      return track;
+    }
+
+    /// <summary>
+    /// Creates a track that consists of a single <see cref="Frame"/>.
+    /// </summary>
+    /// <param name="frame">Assigns to <see cref="Track.frames"/> as a single <see cref="Frame"/>.</param>
+    /// <returns>A new <see cref="Track"/> with a single <see cref="Frame"/>.</returns>
+    protected Track Single(Frame frame) => new Track(this, new Frame[] { frame });
+
+
+    /// <summary>
     /// <para>Shorthand for <see cref="Frame(byte, byte, ushort)"/></para>
     /// <inheritdoc cref="Frame(int, int, int)"/>
     /// </summary>
