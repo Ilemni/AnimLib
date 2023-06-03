30ce8adbf6bc5e572cd9dc5d639969a97b41283d
TwiliChaos <accounts+github@ilemni.com>
2023-06-03 05:09:10 -0700

    /animdebug message when toggled, some overrides

--- AnimLib/Commands/AnimDebugCommand.cs
+++ AnimLib/Commands/AnimDebugCommand.cs
@@ -5,8 +5,20 @@ namespace AnimLib.Commands {
   [UsedImplicitly]
   internal class AnimDebugCommand : ModCommand {
     public override string Command => "animdebug";
+    public override string Usage => "/animdebug";
+    public override string Description => "Toggle AnimLib debug mode, used for /animability.";
+    
     public override CommandType Type => CommandType.Chat;
-    public override void Action(CommandCaller caller, string input, string[] args) 
-      { if(AnimPlayer.Local is not null) AnimPlayer.Local.DebugEnabled ^= true; }
+
+    public override void Action(CommandCaller caller, string input, string[] args) {
+      var player = caller.Player?.GetModPlayer<AnimPlayer>();
+      if(player is not null) {
+        player.DebugEnabled ^= true;
+        caller.Reply(player.DebugEnabled ? EnabledString : DisabledString);
+      }
+    }
+
+    const string EnabledString = "AnimLib debug mode enabled.";
+    const string DisabledString = "AnimLib debug mode disabled.";
   }
 }
