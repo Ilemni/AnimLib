b4b12c8f291fc5a7b33e26895e8c064d8ee6e4d1
pl752 <maximpl752@gmail.com>
2023-06-13 07:34:04 +0500

    Added a thing to hide AnimCharacter sprite when using lilith's necklace (more mounts like this to be added).

---
+++ AnimLib/Compat/DefaultTweals/PlayerHidingMount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+
+namespace AnimLib.Compat.Implementations {
+  /// <summary>
+  /// Hides character sprite when 
+  /// mount which hides vanilla player 
+  /// sprite is active
+  /// </summary>
+  public class PlayerHidingMount : AnimCompatSystem {
+    public readonly HashSet<int> mount_ids = new() { MountID.Wolf };
+
+    public override void PostSetupContent() {
+      GlobalCompatConditions.AddGraphicsDisableCondition(
+        GetStandardPredicate(p => {
+          if (p.mount is not null && p.mount.Active) {
+            return mount_ids.Contains(p.mount.Type);
+          }
+          return false;
+        }
+      ));
+      _initialized = true;
+    }
+  }
+}
