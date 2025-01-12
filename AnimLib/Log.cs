60b4bb16bd7279aefcd37d973714193fa6003fe1
TwiliChaos <accounts+github@ilemni.com>
2025-01-12 11:09:46 -0800

    Fix deadlock caused by Log.Debug

--- AnimLib/Log.cs
+++ AnimLib/Log.cs
@@ -14,10 +14,17 @@ internal static class Log {
   [Conditional("DEBUG")]
   internal static void Debug(string message) {
     AnimLibMod.Instance.Logger.Debug(message);
-    Main.NewText(message, Color.Gray);
+    if (Program.IsMainThread) {
+      Main.NewText(message, Color.Gray);
+    }
   }
 
   internal static void Info(string message) {
     AnimLibMod.Instance.Logger.Info(message);
   }
+
+  internal static bool IsErrorEnabled => AnimLibMod.Instance.Logger.IsErrorEnabled;
+  internal static bool IsWarnEnabled => AnimLibMod.Instance.Logger.IsWarnEnabled;
+  internal static bool IsDebugEnabled => AnimLibMod.Instance.Logger.IsDebugEnabled;
+  internal static bool IsInfoEnabled => AnimLibMod.Instance.Logger.IsInfoEnabled;
 }
