58da1ed3acb57dbc8effbe96a775866dbc191203
TwiliChaos <accounts+github@ilemni.com>
2025-04-01 22:57:13 -0700

    Add Aseprite UserData, arg processing

---
+++ AnimLib/Animations/AnimUserData.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+using AnimLib.Extensions;
+using AsepriteDotNet.Aseprite.Types;
+using AsepriteDotNet.Common;
+
+namespace AnimLib.Animations;
+
+public sealed class AnimUserData {
+  private AnimUserData(string? name, Rgba32? color) :
+    this(name, Unsafe.As<Rgba32?, Color?>(ref color)) {
+  }
+
+  private AnimUserData(string? name, Color? color) {
+    Text = name;
+    Color = color;
+  }
+
+  public static readonly AnimUserData Empty = new(null, null as Color?);
+
+  public readonly string? Text;
+  public readonly Color? Color;
+
+  [MemberNotNullWhen(true, nameof(Text))]
+  public bool HasText => Text is not null;
+
+  [MemberNotNullWhen(true, nameof(Color))]
+  public bool HasColor => Color is not null;
+
+  /// <summary>
+  /// Tries to get a value from the UserData text by key.
+  /// <br/> If <typeparamref name="T"/> is <see langword="bool"/>,
+  /// the value is considered <see langword="true"/> if the key is present and value is missing.
+  /// </summary>
+  /// <param name="key"></param>
+  /// <param name="value"></param>
+  /// <typeparam name="T"></typeparam>
+  /// <returns></returns>
+  public bool TryGetArg<T>(ReadOnlySpan<char> key, [NotNullWhen(true)] out T? value) where T : ISpanParsable<T> =>
+    Text.TryGetArg(key, out value);
+
+  public T? ArgOrDefault<T>(ReadOnlySpan<char> key, T? defaultValue = default) where T : ISpanParsable<T> =>
+    Text.TryGetArg(key, out T? value) ? value : defaultValue;
+
+  public bool HasFlag(ReadOnlySpan<char> key) =>
+    Text.TryGetArg(key, out bool flag) && flag;
+
+  public override string ToString() {
+    return (HasText, HasColor) switch {
+      (true, true) => $"Text = {Text}, Color = {Color}",
+      (true, false) => $"Text = {Text}",
+      (false, true) => $"Color = {Color}",
+      _ => "Empty"
+    };
+  }
+
+  public static implicit operator AnimUserData(AsepriteUserData aseUserData) {
+    return aseUserData is { Text: null, Color: null } ? Empty : new AnimUserData(aseUserData.Text, aseUserData.Color);
+  }
+}
---
+++ AnimLib/Extensions/StringExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using AsepriteDotNet.Aseprite.Types;
+
+namespace AnimLib.Extensions;
+
+/// <summary>
+/// This class
+/// </summary>
+public static class StringExtensions {
+  public static bool TryGetArg<T>(this string? str, ReadOnlySpan<char> key, [NotNullWhen(true)] out T? value)
+    where T : ISpanParsable<T> {
+    if (key.Length == 0) {
+      throw new ArgumentException("Key cannot be empty", nameof(key));
+    }
+
+    if (str is null || str.Length < key.Length) {
+      value = default;
+      return false;
+    }
+
+    foreach (var entry in str.AsSpan().Split(',')) {
+      bool hasValue = entry.SplitKvp(':', out var entryKey, out var entryVal);
+
+      if (!entryKey.Equals(key, StringComparison.InvariantCulture)) {
+        continue;
+      }
+
+      if (hasValue) {
+        return T.TryParse(entryVal, CultureInfo.InvariantCulture, out value);
+      }
+
+      // Treat empty values as though the key is a flag, so value is true
+      if (typeof(T) == typeof(bool)) {
+        value = (T)(object)true;
+        return true;
+      }
+
+      break;
+    }
+
+    value = default;
+    return false;
+  }
+
+  public static bool TryGetArg<T>(this AsepriteUserData userData, ReadOnlySpan<char> key,
+    [NotNullWhen(true)] out T? value)
+    where T : ISpanParsable<T> => TryGetArg(userData.Text, key, out value);
+
+  public static T? ArgOrDefault<T>(this AsepriteUserData userData, ReadOnlySpan<char> key, T? defaultValue = default)
+    where T : ISpanParsable<T> => TryGetArg(userData.Text, key, out T? value) ? value : defaultValue;
+
+
+  private static bool SplitKvp(this ReadOnlySpan<char> kvp, char separator, out ReadOnlySpan<char> key,
+    out ReadOnlySpan<char> value) {
+    int separatorIndex = kvp.IndexOf(separator);
+    if (separatorIndex == -1) {
+      key = kvp.Trim();
+      value = default;
+      return false;
+    }
+
+    key = kvp[..separatorIndex].Trim();
+    value = kvp[(separatorIndex + 1)..].Trim();
+    return true;
+  }
+}
